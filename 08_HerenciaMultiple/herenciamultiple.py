3fdcbc634a4ada285aa1c03f9a87b1aaa28e7506
Physicworld <domingo.cajina@hotmail.com>
2024-06-29 16:06:59 -0600

    expandiendo ejemplo

--- 08_HerenciaMultiple/herenciamultiple.py
+++ 08_HerenciaMultiple/herenciamultiple.py
@@ -1,13 +1,16 @@
 class A:
     def metodo(self):
-        print("Método de A")
+        print("Metodo de A")
+
 
 class B:
     def metodo(self):
-        print("Método de B")
+        print("Metodo de B")
 
-class C(A, B):
-    pass
+
+class C(B, A):
+    def metodo(self):
+        print("Metodo Mixto - C")
 
 obj = C()
-obj.metodo()  # Método de A (MRO - Method Resolution Order)
+obj.metodo()
